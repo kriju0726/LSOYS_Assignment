5773441c12c9cee0b2c0db65da2060f59513f2bc
kumar.krish07022002@gmail.com <kumar.krish07022003@gmail.com>
2025-09-01 11:09:31 +0530

    Adjust

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,7 +1,7 @@
 function ScoreBoard({ highScore, turns }) {
   return (
-    <div className="score-tab">
-      <p style={{ marginLeft: "25px" }}>High Score: {highScore}</p>
+    <div className="score-tab" >
+      <p style={{ margin: "20px" }}>High Score: {highScore}</p>
       <p>Turns: {turns}</p>
     </div>
   );
